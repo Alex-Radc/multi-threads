9f3419e2024bdc44aba0437e5efde0bab63b403d
Alex Radchenko <mor94ok.ua@gmail.com>
2021-04-03 16:03:49 +0300

    add Multi_Thread.c

---
+++ Multi_Thread.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int counter1 = 0;
+int counter2 = 300;
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
